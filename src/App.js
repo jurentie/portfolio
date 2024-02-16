7e7c47fbe3b54efaa9f03a4ff406661fc3ca584a
Justin Rentie <jrentie@talentreef.com>
2024-02-15 22:05:40 -0700

    added all content to one page

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ function App() {
   return (
       <>
         <Navbar />
+        <Landing />
+        <About />
         {/* <AnimatePresence mode="wait"> */}
-          <Routes location={location} key={location.pathname}>
+          {/* <Routes location={location} key={location.pathname}>
             <Route index element={<Landing />} />
             <Route path="/portfolio" element={<Landing />} />
             <Route path="/about" element={<About />} />
-          </Routes>
+          </Routes> */}
         {/* </AnimatePresence> */}
       </>
     )
