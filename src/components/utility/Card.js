8d80595e932f8b02fb5afa4569a8331e74470c1a
Justin Rentie <jrentie@talentreef.com>
2024-02-18 23:42:03 -0700

    fixed projects for mobile view

--- src/components/utility/Card.js
+++ src/components/utility/Card.js
@@ -5,51 +5,79 @@ import { motion } from 'framer-motion'
 
 function Card ({imgSource, title, description, link, index}) {
     return (
-        <motion.div transition={{delay: .5, duration:1, ease:"backInOut"}} initial={{x:-300, opacity: 0}} whileInView={{x: 0, opacity: 1}} viewport={{ once: true }} className="Card">
-            {index % 2 === 0 && (
-                <div className="content">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                    <motion.a 
-                        transition={{duration: 1, repeat:Infinity, repeatType:"reverse"}}
-                        initial={{scale:0.95}}
-                        animate={{scale:1.1}}
-                        // whileHover={{textDecoration:"bold"}}
-                        // whileTap={{scale:0.9}}
-                        href={link} 
-                        target="_"
-                    >
-                        <button>view &rarr;</button>
-                    </motion.a>
+        <motion.div transition={{delay: .5, duration:1, ease:"backInOut"}} initial={{x:-300, opacity: 0}} whileInView={{x: 0, opacity: 1}} viewport={{ once: true }}>
+            <div className="desktop">
+                <div className="Card">
+                    {index % 2 === 0 && (
+                        <div className="content">
+                            <h2>{title}</h2>
+                            <p>{description}</p>
+                            <motion.a 
+                                transition={{duration: 1, repeat:Infinity, repeatType:"reverse"}}
+                                initial={{scale:0.95}}
+                                animate={{scale:1.1}}
+                                // whileHover={{textDecoration:"bold"}}
+                                // whileTap={{scale:0.9}}
+                                href={link} 
+                                target="_"
+                            >
+                                <button>view &rarr;</button>
+                            </motion.a>
+                        </div>
+                    )}
+                    <div className="image">
+                        <motion.a 
+                            whileHover={{scale:1.1}}
+                            whileTap={{scale:0.9}}
+                            href={link} 
+                            target="_"
+                        >
+                            <img src={process.env.PUBLIC_URL + imgSource} alt={title}></img>
+                        </motion.a>
+                    </div>
+                    {index % 2 !== 0 && (
+                        <div className="content">
+                            <h2>{title}</h2>
+                            <p>{description}</p>
+                            <motion.a 
+                                transition={{duration: 1, repeat:Infinity, repeatType:"reverse"}}
+                                initial={{scale:0.95}}
+                                animate={{scale:1.1}}
+                                // whileHover={{scale:1.1}}
+                                // whileTap={{scale:0.9}}
+                                href={link} 
+                                target="_"
+                            >
+                                <button>view &rarr;</button>
+                            </motion.a>
+                        </div>
+                    )}
                 </div>
-            )}
-            <div className="image">
-                <motion.a 
-                    whileHover={{scale:1.1}}
-                    whileTap={{scale:0.9}}
-                    href={link} 
-                    target="_"
-                >
-                    <img src={process.env.PUBLIC_URL + imgSource} alt={title}></img>
-                </motion.a>
             </div>
-            {index % 2 !== 0 && (
-                <div className="content">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                    <motion.a 
-                        transition={{duration: 1, repeat:Infinity, repeatType:"reverse"}}
-                        initial={{scale:0.95}}
-                        animate={{scale:1.1}}
-                        // whileHover={{scale:1.1}}
-                        // whileTap={{scale:0.9}}
-                        href={link} 
-                        target="_"
-                    >
-                        <button>view &rarr;</button>
-                    </motion.a>
+            <div className="mobile">
+                <div className="Card">
+                    <div className="image">
+                        <a href={link} target="_">
+                            <img src={process.env.PUBLIC_URL + imgSource} alt={title}></img>
+                        </a>
+                    </div>
+                    <div className="content">
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <motion.a 
+                            transition={{duration: 1, repeat:Infinity, repeatType:"reverse"}}
+                            initial={{scale:0.95}}
+                            animate={{scale:1.1}}
+                            // whileHover={{scale:1.1}}
+                            // whileTap={{scale:0.9}}
+                            href={link} 
+                            target="_"
+                        >
+                            <button>view &rarr;</button>
+                        </motion.a>
+                    </div>
                 </div>
-            )}
+            </div>
         </motion.div>
     )
 }
